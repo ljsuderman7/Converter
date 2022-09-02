02243b3923de4385ae1bc8623b75565f3f319767
Leighton <ljsuderman7@gmail.com>
2022-09-02 14:43:59 -0400

    Added UI, list for different conversion types (Temperature, Length)

--- app/src/main/java/ca/lsuderman/converter/MainActivity.java
+++ app/src/main/java/ca/lsuderman/converter/MainActivity.java
@@ -3,12 +3,37 @@ package ca.lsuderman.converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnConvert;
+    private TextView txtTopConvert, txtBottomConvert;
+    private AutoCompleteTextView txtTopUnit, txtBottomUnit, txtConversion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnConvert = findViewById(R.id.btnConvert);
+        txtBottomConvert = findViewById(R.id.txtBottomConvert);
+        txtTopConvert= findViewById(R.id.txtTopConvert);
+        txtBottomUnit = findViewById(R.id.txtBottomUnit);
+        txtTopUnit = findViewById(R.id.txtTopUnit);
+        txtConversion = findViewById(R.id.txtConversion);
+
+        String[] conversionOptions = {"Temperature", "Length"};
+        ArrayAdapter<String> conversionAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, conversionOptions);
+        txtConversion.setAdapter(conversionAdapter);
+
+        //TODO: add more options
+        String[] TemperatureOptions = {"Celsius", "Fahrenheit", "Kelvin"};
+        String[] lengthOptions = {"Centimetres", "Metres", "Inches", "Feet", }; //TODO: add more
+
+
     }
 }
