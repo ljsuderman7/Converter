5f886338282abb400f3abdd3ece76aa30d95aa82
Leighton <ljsuderman7@gmail.com>
2022-09-06 14:33:47 -0400

    added more conversion options (not implemented)

--- app/src/main/java/ca/lsuderman/converter/MainActivity.java
+++ app/src/main/java/ca/lsuderman/converter/MainActivity.java
@@ -30,15 +30,27 @@ public class MainActivity extends AppCompatActivity {
         txtConversion = findViewById(R.id.txtConversion);
 
         //TODO: add more conversion options
-        String[] conversionOptions = {"Temperature", "Length"};
+        String[] conversionOptions = {"Temperature", "Length", "Mass", "Area", "Data", "Time"};
         ArrayAdapter<String> conversionAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, conversionOptions);
         txtConversion.setAdapter(conversionAdapter);
 
         String[] temperatureOptions = {"Celsius", "Fahrenheit", "Kelvin"};
         ArrayAdapter<String> temperatureAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, temperatureOptions);
 
-        String[] lengthOptions = {"Centimetres", "Metres", "Inches", "Feet",}; //TODO: add more lengths
+        String[] lengthOptions = {"Millimetres", "Centimetres", "Metres", "Kilometers", "Inches", "Feet", "Yards", "Miles"};
         ArrayAdapter<String> lengthAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, lengthOptions);
+
+        String[] massOptions = {"Grams", "Kilograms", "Ounces", "Pounds"};
+        ArrayAdapter<String> massAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, massOptions);
+
+        String[] areaOptions = {"Acres", "Hectares", "Square centimetres", "Square feet", "Square inches", "Square metres"};
+        ArrayAdapter<String> areaAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, areaOptions);
+
+        String[] dataOptions = {"Bits", "Bytes", "Kilobytes", "Megabytes", "Gigabytes", "Terabytes"};
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, dataOptions);
+
+        String[] timeOptions = {"Milliseconds", "Seconds", "Minutes", "Hours", "Days"};
+        ArrayAdapter<String> timeAdapter = new ArrayAdapter<String>(MainActivity.this, R.layout.list_item, timeOptions);
 
 
         txtConversion.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -52,6 +64,22 @@ public class MainActivity extends AppCompatActivity {
                     case "Length":
                         txtUnitFrom.setAdapter(lengthAdapter);
                         txtUnitTo.setAdapter(lengthAdapter);
+                        break;
+                    case "Mass":
+                        txtUnitFrom.setAdapter(massAdapter);
+                        txtUnitTo.setAdapter(massAdapter);
+                        break;
+                    case "Area":
+                        txtUnitFrom.setAdapter(areaAdapter);
+                        txtUnitTo.setAdapter(areaAdapter);
+                        break;
+                    case "Data":
+                        txtUnitFrom.setAdapter(dataAdapter);
+                        txtUnitTo.setAdapter(dataAdapter);
+                        break;
+                    case "Time":
+                        txtUnitFrom.setAdapter(timeAdapter);
+                        txtUnitTo.setAdapter(timeAdapter);
                         break;
                 }
             }
@@ -70,15 +98,28 @@ public class MainActivity extends AppCompatActivity {
                         conversion = temperatureConversion(convertFrom, convertTo ,numberToConvert);
                         break;
                     case "Length":
-                        lengthConversion();
+                        conversion = lengthConversion(convertFrom, convertTo ,numberToConvert);
+                        break;
+                    case "Mass":
+                        conversion = lengthConversion(convertFrom, convertTo ,numberToConvert);
+                        break;
+                    case "Area":
+                        conversion = lengthConversion(convertFrom, convertTo ,numberToConvert);
+                        break;
+                    case "Data":
+                        conversion = lengthConversion(convertFrom, convertTo ,numberToConvert);
+                        break;
+                    case "Time":
+                        conversion = lengthConversion(convertFrom, convertTo ,numberToConvert);
                         break;
                     default:
                         //TODO: Make error snackbar
                         break;
                 }
 
+                // Displays the converted number with a decimal, only when necessary
+                // TODO: display unit short form instead of whole word
                 String conversionMessage;
-
                 if (conversion % 1 == 0) {
                     String numberToConvertString = String.valueOf(numberToConvert);
                     String convertedNumberString = String.valueOf(conversion);
@@ -133,7 +174,11 @@ public class MainActivity extends AppCompatActivity {
         return conversion;
     }
 
-    private void lengthConversion(){
+    private double lengthConversion(String convertFrom, String convertTo, double numberToConvert){
+        double conversion = 0.0;
 
+
+
+        return conversion;
     }
 }
